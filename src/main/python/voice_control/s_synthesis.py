d1a5bd378d55950ddc087d990d83debf34140f7c
Daniel Bielaszka <danielbielaszka@gmail.com>
2023-04-03 14:58:10 +0200

    s_synthesis speak(s: string) function

--- src/main/python/voice_control/s_synthesis.py
+++ src/main/python/voice_control/s_synthesis.py
@@ -1,10 +1,6 @@
-# import gtts
-# from playsound import playsound
-# tts = gtts.gTTS(text='Witam w szachach ', lang='pl')
-# tts.save("welcome.mp3")
-
 
 import pyttsx3
+
 def change_voice(engine, language):
     for voice in engine.getProperty('voices'):
         if language in voice.languages :
@@ -12,8 +8,10 @@ def change_voice(engine, language):
             return True
 
 
-engine = pyttsx3.init()
-change_voice(engine, 'pl_PL')
-txt = "W Szczebrzeszynie chrząszcz brzmi w trzcinie"
-engine.say(txt)
-engine.runAndWait()
+
+def speak(text):
+    engine = pyttsx3.init()
+    change_voice(engine, 'pl_PL')  
+    engine.say(text)
+    engine.runAndWait()
+
